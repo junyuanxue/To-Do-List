b1a4f56c998d8ff86f9b18389a39417c8f066c81
Murilo Dal Ri <murilodalri@gmail.com>
2016-04-18 14:48:02 +0100

    added todo text and remove todo

--- controllers.js
+++ controllers.js
@@ -2,4 +2,13 @@ toDoApp.controller('ToDoController', function() {
   this.todos = [
     { text: "Throw away banana", completed: true },
     { text: "Squash pineapple", completed: false }];
+
+    this.addToDo = function() {
+      this.todos.push({ text: this.toDoText, completed: false });
+      this.toDoText = '';
+    };
+
+    this.removeToDo = function() {
+      this.todos.pop();
+    };
 });
