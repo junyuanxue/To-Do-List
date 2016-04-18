c121adde41f48d558d4bf6fba5dba30fcbba29d7
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-18 18:24:21 +0100

    karma unit test passing

---
+++ app/js/controllers/ToDoController.js
@@ -0,0 +1,3 @@
+toDoApp.controller('ToDoController', [function () {
+  this.toDo = "Feck off Protractor!";
+}]);
--- test/todo-spec.js
+++ test/todo-spec.js
@@ -1,6 +1,13 @@
 describe('Todo app', function(){
-  it('displays hello world', function(){
+  it('displays the title', function(){
     browser.get('/app');
     expect(browser.getTitle()).toEqual('Todos App');
+  });
+
+  it('lists a todo', function() {
+    browser.get('/app');
+
+    var toDoList = element.all(by.repeater('todo in controller.todos'));
+    expect(toDoList.get(0).getText()).toEqual('Feck off Protractor!');
   });
 });
