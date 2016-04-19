787d97f7b7c14d31809962084290dd83e59fff31
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-19 23:13:20 +0100

    added karma spec reporter

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -71,7 +71,18 @@ module.exports = function(config) {
 
     plugins : [
             'karma-chrome-launcher',
-            'karma-jasmine'
-    ]
+            'karma-jasmine',
+            "karma-spec-reporter"
+    ],
+
+    reporters: ['spec'],
+    specReporter: {
+      maxLogLines: 5,         // limit number of lines logged per test
+      suppressErrorSummary: true,  // do not print error summary
+      suppressFailed: false,  // do not print information about failed tests
+      suppressPassed: false,  // do not print information about passed tests
+      suppressSkipped: true,  // do not print information about skipped tests
+      showSpecTiming: false // print the time elapsed for each spec
+    },
   })
 }
