243518e4b3a23238f61f33076c402b4486cf8520
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-20 11:26:16 +0100

    changed path in feature spec

--- test/e2e/todoFeatures.js
+++ test/e2e/todoFeatures.js
@@ -1,13 +1,13 @@
 describe('Todos', function() {
   it('has a title', function() {
     // We don't need to put in the full url as we set baseUrl in our config
-    browser.get('/');
+    browser.get('/app');
     expect(browser.getTitle()).toEqual('Todos App');
   });
 
   it('has todo1', function(){
-    browser.get('/');
+    browser.get('/app');
     var todo = $('#todo')
-    expect(todo.getText()).toEqual('todo1')
+    expect(todo.getText()).toEqual('ToDo1')
   });
 });
